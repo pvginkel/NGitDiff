f445fce0251f89286206b55829fcf057fbc51eeb
Pieter van Ginkel <pvginkel@gmail.com>
2013-08-18 09:12:47 +0200

    Initial transformation from the NGit project at https://github.com/mono/ngit.

---
+++ NGitDiff/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NGit
+{
+    static class Constants
+    {
+        public static readonly string CHARACTER_ENCODING = "UTF-8";
+        public static readonly Encoding CHARSET;
+
+        static Constants()
+        {
+            CHARSET = Sharpen.Extensions.GetEncoding(CHARACTER_ENCODING);
+        }
+    }
+}
